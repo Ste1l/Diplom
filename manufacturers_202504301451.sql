5400b1ce1880e5de0c52316c88a205c4ebc5344f
Ste1l <caxapo4ek2903@mail.ru>
2025-05-15 11:42:16 +0400

    Добавил sql запросы для бд

---
+++ manufacturers_202504301451.sql
@@ -0,0 +1,4 @@
+INSERT INTO public.manufacturers (manufacturer_name,address,phone_number,inn,kpp,account_number) VALUES
+	 ('Вертекс','Улица Пушкина дом Колотушкина','79508288908','12345678900','12345678900','ВГ010101'),
+	 ('C.К. Терапия С.А.','','','','',''),
+	 ('Биосинтез','','','','','');
